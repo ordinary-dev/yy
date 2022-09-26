f69fcca2189ccde58384f4eefa8a4742d7d4485f
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-09-26 11:44:29 +0500

    Photo upload functionality

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Router from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, FormEvent } from 'react'
 import { withIronSessionSsr } from "iron-session/next"
 import { sessionOptions } from "../lib/session"
 import styles from '../styles/admin.module.css'
@@ -41,6 +41,7 @@ const AdminPage: NextPage<PageProps> = props => {
         <div>List of photos:</div>
         { props.photos.map((photo, index) =>
             <div key={ index }>{ index }. { photo.descriptionEn }</div>) }
+        <UploadForm />
         <button onClick={(e) => logout()}>Logout</button>
     </div>
     
@@ -53,6 +54,51 @@ const logout = async() => {
     const response = await fetch('/api/logout')
     const res = await response.json()
     if ( res.ok ) Router.push('/login')
+}
+
+const UploadForm = () => {
+    return <>
+        <div>Upload new photo</div>
+        <form onSubmit={ e => handleSubmit(e) }>
+            <input type="file"
+                   accept="image/png, image/jpeg, image/webp"
+                   name="image"
+                   required />
+            <button type="submit">Submit</button>
+        </form>
+    </>
+}
+
+interface UploadForm extends HTMLFormElement {
+    image: HTMLInputElement
+}
+
+const handleSubmit = async(e: FormEvent) => {
+    e.preventDefault()
+
+    const target = e.target as UploadForm
+    if (!target.image.files || target.image.files.length < 1)
+        throw new Error("No files selected")
+
+    const image = target.image.files[0]
+    const formData = new FormData()
+    formData.append("image", image)
+
+    const options = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json'
+        },
+        body: formData
+    }
+
+    const res = await fetch("/api/upload", options)
+    const result = await res.json()
+        
+    // Server returned an error
+    if (!result.ok) throw new Error(result.message)
+
+    target.reset()
 }
 
 export default AdminPage
---
+++ pages/api/upload.ts
@@ -0,0 +1,80 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { IncomingForm, Files, File } from 'formidable'
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { sessionOptions } from '../../lib/session'
+import { PrismaClient } from '@prisma/client'
+import fs from 'fs'
+
+// disable the default body parser
+export const config = {
+    api: {
+        bodyParser: false,
+    }
+}
+
+type MyResponse = {
+    ok: boolean
+    message: string
+}
+
+export default withIronSessionApiRoute(handle, sessionOptions)
+    
+async function handle(req: NextApiRequest, res: NextApiResponse<MyResponse>) {
+    try {
+        // Check request method
+        if (req.method !== 'POST') throw new Error("Wrong request method")
+
+        // Check auth
+        const user = req.session.user
+        if (!user) throw new Error("You are not authorized")
+    
+        // Parse form
+        const form = new IncomingForm()
+        const files = await new Promise<Files>((resolve, reject) => {
+            form.parse(req, (err, _fields, files) => {
+                if (err) reject(err)
+                else resolve(files)
+            })
+        })
+
+        const imageFile = isFileArray(files.image) ? files.image[0] : files.image
+            
+        // Create folder structure
+        const ext = getExtensionFromMimeType(imageFile)
+        if (!ext) throw new Error("Wrong mime type")
+
+        // Save photo
+        const prisma = new PrismaClient()
+        const photo = await prisma.photo.create({
+            data: {}
+        })
+        if (!photo) throw new Error("Cannot save photo")
+        const imageDir = `photos/${photo.id}`
+        if (!fs.existsSync(imageDir)) {
+            await fs.promises.mkdir(imageDir, { recursive: true } )
+        }
+
+        // Write file
+        const imageWritePath = `${imageDir}/original.${ext}`
+        const image = await fs.promises.readFile(imageFile.filepath)
+        await fs.promises.writeFile(imageWritePath, image)
+        
+        res.status(200).json({ ok: true, message: 'ok' })
+    }
+    catch(err) {
+        const msg = err instanceof Error && err.message ? err.message : "Unknown error"
+        res.status(400).json({ ok: false, message: msg })
+    }
+}
+
+const isFileArray = (fa: unknown): fa is Array<File> => {
+    return Array.isArray(fa)
+}
+
+const getExtensionFromMimeType = (imageFile: File) => {
+    const mt = imageFile.mimetype
+    if (mt === 'image/jpeg') return 'jpg'
+    if (mt === 'image/png')  return 'png'
+    if (mt === 'image/webp') return 'webp'
+    return null
+}
