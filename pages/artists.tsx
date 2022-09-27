a554acbd72e32b0aba953304229609f1735b969b
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-09-27 10:44:25 +0500

    Add an image for an unfinished page

--- pages/artists.tsx
+++ pages/artists.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 
 const Artists: NextPage = props => {
     return <>
+        <Image src="/work-in-progress.png" width="500" height="460" alt="Work in progress" />
     </>
 }
 
