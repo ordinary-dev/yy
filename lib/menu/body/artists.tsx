2743e7f7d670c095799d7b629952382d21d8eb47
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-10-23 17:50:08 +0500

    Hide "Error" and "Loading" text in artists section if it's closed

--- lib/menu/body/artists.tsx
+++ lib/menu/body/artists.tsx
@@ -1,4 +1,4 @@
-import useSWR from 'swr'
+import useSWR from "swr"
 import { useRouter } from "next/router"
 import Link from "next/link"
 import { useState, useEffect } from "react"
@@ -9,10 +9,10 @@ import { ArtistsAPIGoodResponse, ArtistWithExtras } from "pages/api/artists"
 
 const Artists = () => {
     const [isOpen, setIsOpen] = useState(false)
-    
+
     // Variables needed to track page switching
     const router = useRouter()
-    
+
     // This function is run every time the url changes.
     // It shows the menu if the user is viewing the artist's page, and hides it otherwise.
     useEffect(() => {
@@ -20,7 +20,7 @@ const Artists = () => {
             setIsOpen(router.asPath.startsWith("/artists"))
         }
     }, [router.isReady, router.asPath])
-    
+
     return (
         <>
             <StyledLink
@@ -34,68 +34,102 @@ const Artists = () => {
     )
 }
 
-const Roles = ({isOpen}: {isOpen: boolean}) => {
+const Roles = ({ isOpen }: { isOpen: boolean }) => {
     const router = useRouter()
-    const { data, error } = useSWR<ArtistsAPIGoodResponse, Error>("/api/artists")
-    
-    if (error) return (<div>ERROR</div>)
-    if (!data) return (<div>LOADING</div>)
+    const { data, error } = useSWR<ArtistsAPIGoodResponse, Error>(
+        "/api/artists"
+    )
 
     const usedRoles = new Set()
 
-    if (isOpen) return (
-        <>
-            { data.artists.map(artist => {
-                // If role wasn't shown before
-                if (!usedRoles.has(artist.role.url)) {
-                    // Save role
-                    usedRoles.add(artist.role.url)
-                    // Return a link to the first person with this role
-                    return (
-                        <>
-                        <Link href={`/artists/${artist.role.url}/${artist.person.url}`} passHref>
-                            <StyledLink light>
-                                <En>{artist.role.nameEn.toUpperCase()}</En>
-                                <Ru>{artist.role.nameRu.toUpperCase()}</Ru>
-                            </StyledLink>
-                        </Link>
-                        <People artists={data.artists}
-                                role={artist.role}
-                                isVisible={router.query.role === artist.role.url} />
-                        </>
-                    )
-                }
-                // Return nothing if the role was already shown
-                return <></>
-            })}
-        </>
-    )
+    if (isOpen) {
+        if (error) return <div>ERROR</div>
+        if (!data) return <div>LOADING</div>
+        return (
+            <>
+                {data.artists.map(artist => {
+                    // If role wasn't shown before
+                    if (!usedRoles.has(artist.role.url)) {
+                        // Save role
+                        usedRoles.add(artist.role.url)
+                        // Return a link to the first person with this role
+                        return (
+                            <>
+                                <Link
+                                    href={`/artists/${artist.role.url}/${artist.person.url}`}
+                                    passHref>
+                                    <StyledLink light>
+                                        <En>
+                                            {artist.role.nameEn.toUpperCase()}
+                                        </En>
+                                        <Ru>
+                                            {artist.role.nameRu.toUpperCase()}
+                                        </Ru>
+                                    </StyledLink>
+                                </Link>
+                                <People
+                                    artists={data.artists}
+                                    role={artist.role}
+                                    isVisible={
+                                        router.query.role === artist.role.url
+                                    }
+                                />
+                            </>
+                        )
+                    }
+                    // Return nothing if the role was already shown
+                    return <></>
+                })}
+            </>
+        )
+    }
     return <></>
 }
 
-const People = ({artists, role, isVisible}: { artists: ArtistWithExtras[], role: Role, isVisible: boolean }) => {
+const People = ({
+    artists,
+    role,
+    isVisible,
+}: {
+    artists: ArtistWithExtras[]
+    role: Role
+    isVisible: boolean
+}) => {
     const usedPersons = new Set()
     const router = useRouter()
-    if (isVisible) return (
-        <>
-            {artists.map(artist => {
-                if (artist.role.id === role.id) {
-                    if (!usedPersons.has(artist.person.id)) {
-                        usedPersons.add(artist.person.id)
-                        return (
-                            <Link key={artist.person.id} href={`/artists/${role.url}/${artist.person.url}`} passHref>
-                                <StyledLink light isActive={router.query.name === artist.person.url} >
-                                    <En>{artist.person.nameEn.toUpperCase()}</En>
-                                    <Ru>{artist.person.nameRu.toUpperCase()}</Ru>
-                                </StyledLink>
-                            </Link>
-                        )
+    if (isVisible)
+        return (
+            <>
+                {artists.map(artist => {
+                    if (artist.role.id === role.id) {
+                        if (!usedPersons.has(artist.person.id)) {
+                            usedPersons.add(artist.person.id)
+                            return (
+                                <Link
+                                    key={artist.person.id}
+                                    href={`/artists/${role.url}/${artist.person.url}`}
+                                    passHref>
+                                    <StyledLink
+                                        light
+                                        isActive={
+                                            router.query.name ===
+                                            artist.person.url
+                                        }>
+                                        <En>
+                                            {artist.person.nameEn.toUpperCase()}
+                                        </En>
+                                        <Ru>
+                                            {artist.person.nameRu.toUpperCase()}
+                                        </Ru>
+                                    </StyledLink>
+                                </Link>
+                            )
+                        }
                     }
-                }
-                return <></>
-            })}
-        </>
-    )
+                    return <></>
+                })}
+            </>
+        )
     return <></>
 }
 
