95ab0bfa895ab5090199cbc9eb046b4d006abefc
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-10-26 14:55:21 +0500

    Add shynet integration

---
+++ lib/tracking/index.tsx
@@ -0,0 +1,45 @@
+import Script from "next/script"
+import Image from "next/image"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import styles from "./tracking.module.css"
+
+// Shynet - privacy-friendly web analytics
+// https://github.com/milesmcc/shynet
+const Tracking = () => {
+    const router = useRouter()
+
+    const sendData = () => {
+        if (window["Shynet"]) window["Shynet"].newPageLoad()
+    }
+
+    useEffect(() => {
+        sendData()
+    }, [router.asPath, router.locale])
+
+    return (
+        <div className={styles.hidden}>
+            <Script
+                src="https://stats.altweb.tech/ingress/494db2b8-6ed0-4bb2-b7a6-d2fb9b63a9fd/script.js"
+                onLoad={() => sendData()}
+            />
+            <noscript>
+                <Image
+                    width="1px"
+                    height="1px"
+                    src="https://stats.altweb.tech/ingress/494db2b8-6ed0-4bb2-b7a6-d2fb9b63a9fd/pixel.gif"
+                    unoptimized={true}
+                    alt="Pixel"
+                />
+            </noscript>
+        </div>
+    )
+}
+
+export default Tracking
+
+declare global {
+    interface Window {
+        Shynet: any
+    }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,25 +4,28 @@ import "@fontsource/montserrat/300.css"
 import type { AppProps } from "next/app"
 import Head from "next/head"
 import { SWRConfig } from "swr"
+import Tracking from "lib/tracking"
 import Layout from "lib/menu"
-
 import "styles/globals.css"
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-    <SWRConfig
-        value={{
-            refreshInterval: 60000,
-            fetcher: (resource, init) =>
-                fetch(resource, init).then(res => res.json()),
-        }}>
-        <Head>
-            <Favicons />
-        </Head>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    </SWRConfig>
-)
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <SWRConfig
+            value={{
+                refreshInterval: 60000,
+                fetcher: (resource, init) =>
+                    fetch(resource, init).then(res => res.json()),
+            }}>
+            <Head>
+                <Favicons />
+            </Head>
+            <Tracking />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </SWRConfig>
+    )
+}
 
 const Favicons = () => (
     <>
