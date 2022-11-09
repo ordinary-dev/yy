dc95482268b6d9f3a744b1ebbb390870a96c5a1f
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-11-10 02:07:22 +0500

    Convert logo to svg format

--- lib/menu/logo.tsx
+++ lib/menu/logo.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link"
 import Image from "next/image"
-import YY from "public/logo.png"
 import styles from "./logo.module.css"
 
 // YY
 const Logo = () => {
     return (
         <Link href="/" className={styles.Logo}>
-            <Image src={YY} alt="Logo" placeholder="blur" fill />
+            <Image src="/logo.svg" alt="Logo" fill />
         </Link>
     )
 }
