769bf67617730f3050165da372e296212a75c633
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-11-10 01:36:35 +0500

    Speed up the rendering of pages with information about the company

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,89 +1,74 @@
-import type { NextPage } from "next"
-import { ReactNode } from "react"
-import { Ru, En } from "lib/interpreter"
+import type { NextPage, GetStaticProps } from "next"
 import Meta from "lib/meta"
 import styles from "styles/info.module.css"
 
-const About: NextPage = () => {
-    return (
-        <div className={styles.Info}>
-            <Meta title="About" />
-            <div className={styles.Column}>
-                <div className={styles.Title + " " + styles.FirstTitle}>
-                    <En>ABOUT</En>
-                    <Ru>О НАС</Ru>
-                </div>
-                <Text>
-                    <En>
-                        YY STUDIOS IS A FULL SERVICE AND TALENT REPRESENTATION
-                        AGENCY, A MODERN REPRESENTATIVE, ABLE TO SATISFY ALL THE
-                        NEEDS OF THE ARTIST AND THE CLIENT. THE AGENCY WAS
-                        CREATED TO OFFER AN ALTERNATIVE PLATFORM FOR
-                        SELF-EXPRESSION SHOWING ARTISTS IN THE CONTEXT OF A
-                        MODERN PHOTOGRAPHIC GENRE ORIGINING IN RUSSIA.
-                    </En>
-                    <Ru>
-                        YY STUDIOS - ЭТО АГЕНТСТВО ПОЛНОГО ЦИКЛА И РЕПРЕЗЕНТАЦИИ
-                        ТАЛАНТОВ, СОВРЕМЕННЫЙ ПРЕДСТАВИТЕЛЬ, СПОСОБНЫЙ
-                        УДОВЛЕТВОРИТЬ ВСЕ ПОТРЕБНОСТИ ХУДОЖНИКА И КЛИЕНТА.
-                        АГЕНТСТВО БЫЛО СОЗДАНО, ЧТОБЫ ПРЕДЛОЖИТЬ АЛЬТЕРНАТИВНУЮ
-                        ПЛАТФОРМУ ДЛЯ САМОВЫРАЖЕНИЯ, ДЕМОНСТРИРУЯ ХУДОЖНИКОВ В
-                        КОНТЕКСТЕ СОВРЕМЕННОГО ФОТОГРАФИЧЕСКОГО ЖАНРА,
-                        ЗАРОЖДАЮЩЕГОСЯ В РОССИИ.
-                    </Ru>
-                </Text>
-            </div>
-            <div className={styles.Column}>
-                <div className={styles.Title}>
-                    <En>OUR SERVICES</En>
-                    <Ru>НАШИ СЕРВИСЫ</Ru>
-                </div>
-                <Text>
-                    <En>PHOTO PRODUCTION</En>
-                    <Ru>ФОТОПРОИЗВОДСТВО</Ru>
-                </Text>
-
-                <Text>
-                    <En>ART DIRECTION</En>
-                    <Ru>АРТ-ДИРЕКЦИЯ</Ru>
-                </Text>
-
-                <Text>
-                    <En>BRAND CONTENT</En>
-                    <Ru>СОДЕРЖАНИЕ БРЕНДА</Ru>
-                </Text>
-
-                <Text>
-                    <En>ART CONSULTATION</En>
-                    <Ru>АРТ КОНСУЛЬТАЦИЯ</Ru>
-                </Text>
-
-                <Text>
-                    <En>CASTING</En>
-                    <Ru>КАСТИНГ</Ru>
-                </Text>
-
-                <Text>
-                    <En>ARTISTS MANAGMENT</En>
-                    <Ru>МЕНЕДЖМЕНТ АРТИСТОВ</Ru>
-                </Text>
-
-                <Text>
-                    <En>E-COMMERCE</En>
-                    <Ru>ЭЛЕКТРОННАЯ КОММЕРЦИЯ</Ru>
-                </Text>
-
-                <Text>
-                    <En>RETOUCHING</En>
-                    <Ru>РЕТУШЬ</Ru>
-                </Text>
-            </div>
-        </div>
-    )
+type PageProps = {
+    title: string
+    description: string
+    ourServices: string
+    photoProd: string
+    artDir: string
+    brandContent: string
+    artConsultation: string
+    casting: string
+    artists: string
+    eCommerce: string
+    retouching: string
 }
 
-const Text = ({ children }: { children: ReactNode }) => {
-    return <div className={styles.Text}>{children}</div>
+// Generate locale
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+    const props: PageProps =
+        locale === "en"
+            ? {
+                  title: "About",
+                  description:
+                      "YY STUDIOS IS A FULL SERVICE AND TALENT REPRESENTATION AGENCY, A MODERN REPRESENTATIVE, ABLE TO SATISFY ALL THE NEEDS OF THE ARTIST AND THE CLIENT. THE AGENCY WAS CREATED TO OFFER AN ALTERNATIVE PLATFORM FOR SELF-EXPRESSION SHOWING ARTISTS IN THE CONTEXT OF A MODERN PHOTOGRAPHIC GENRE ORIGINING IN RUSSIA.",
+                  ourServices: "OUR SERVICES",
+                  photoProd: "PHOTO PRODUCTION",
+                  artDir: "ART DIRECTION",
+                  brandContent: "BRAND CONTENT",
+                  artConsultation: "ART CONSULTATION",
+                  casting: "CASTING",
+                  artists: "ARTISTS MANAGEMENT",
+                  eCommerce: "E-COMMERCE",
+                  retouching: "RETOUCHING",
+              }
+            : {
+                  title: "О нас",
+                  description:
+                      "YY STUDIOS - ЭТО АГЕНТСТВО ПОЛНОГО ЦИКЛА И РЕПРЕЗЕНТАЦИИ ТАЛАНТОВ, СОВРЕМЕННЫЙ ПРЕДСТАВИТЕЛЬ, СПОСОБНЫЙ УДОВЛЕТВОРИТЬ ВСЕ ПОТРЕБНОСТИ ХУДОЖНИКА И КЛИЕНТА. АГЕНТСТВО БЫЛО СОЗДАНО, ЧТОБЫ ПРЕДЛОЖИТЬ АЛЬТЕРНАТИВНУЮ ПЛАТФОРМУ ДЛЯ САМОВЫРАЖЕНИЯ, ДЕМОНСТРИРУЯ ХУДОЖНИКОВ В КОНТЕКСТЕ СОВРЕМЕННОГО ФОТОГРАФИЧЕСКОГО ЖАНРА, ЗАРОЖДАЮЩЕГОСЯ В РОССИИ.",
+                  ourServices: "НАШИ СЕРВИСЫ",
+                  photoProd: "ПРОДАКШН СЪЕМОК",
+                  artDir: "АРТ ДИРЕКШН",
+                  brandContent: "КОНТЕНТ ДЛЯ БРЕНДОВ",
+                  artConsultation: "АРТ КОНСУЛЬТАЦИИ",
+                  casting: "КАСТИНГ МОДЕЛЕЙ",
+                  artists: "БУКИНГ ТАЛАНТОВ",
+                  eCommerce: "E-COMMERCE",
+                  retouching: "РЕТУШЬ",
+              }
+    return { props }
+}
+
+const About: NextPage<PageProps> = props => {
+    return (
+        <div className={styles.Info}>
+            <Meta title={props.title} />
+            <h2>{props.title.toUpperCase()}</h2>
+            <p>{props.description}</p>
+
+            <h2>{props.ourServices}</h2>
+            <p>{props.photoProd}</p>
+            <p>{props.artDir}</p>
+            <p>{props.brandContent}</p>
+            <p>{props.artConsultation}</p>
+            <p>{props.casting}</p>
+            <p>{props.artists}</p>
+            <p>{props.eCommerce}</p>
+            <p>{props.retouching}</p>
+        </div>
+    )
 }
 
 export default About
