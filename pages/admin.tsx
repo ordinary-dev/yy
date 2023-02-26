7f81606e862358dd418870abc795596a529c3b49
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-02-26 22:06:22 +0500

    Admin page: check authorization on server, simplify logout API

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,63 +1,42 @@
 import type { NextPage } from "next"
 import Router from "next/router"
-import { useEffect } from "react"
 import { withIronSessionSsr } from "iron-session/next"
 import { sessionOptions } from "lib/session"
-import styles from "styles/admin.module.css"
 import ListOfPhotos from "lib/admin/photos"
 import PasswordManager from "lib/admin/password"
 import Artists from "lib/admin/artists"
-import { LogoutAPI } from "./api/logout"
 import Meta from "lib/meta"
-
-type PageProps = {
-    isAdmin: boolean
-}
+import styles from "styles/admin.module.css"
 
 export const getServerSideProps = withIronSessionSsr(async (context) => {
     const user = context.req.session.user
 
-    const props: PageProps = {
-        isAdmin: user !== undefined,
+    if (user === undefined) return {
+        redirect: {
+            destination: '/login',
+            permanent: false,
+        }
     }
-    return { props }
+
+    return { props: {} }
 }, sessionOptions)
 
-const AdminPage: NextPage<PageProps> = (props) => {
-    // Redirect to login page
-    // If user is not authorized
-    useEffect(() => {
-        if (!props.isAdmin) {
-            setTimeout(() => Router.push("/login"), 1000)
-        }
-    }, [props.isAdmin])
-
-    if (props.isAdmin)
-        return (
-            <div className={styles.Container}>
-                <Meta title="Admin" />
-                <div>Hi, admin</div>
-                <ListOfPhotos />
-                <Artists />
-                <PasswordManager />
-                <button className={styles.Button} onClick={() => logout()}>
-                    Logout
-                </button>
-            </div>
-        )
-
-    return (
-        <div className={styles.Container}>
-            <Meta title="Redirecting" />
-            You are not the admin
-        </div>
-    )
-}
+const AdminPage: NextPage = () => (
+    <div className={styles.Container}>
+        <Meta title="Admin" />
+        <div>Hi, admin</div>
+        <ListOfPhotos />
+        <Artists />
+        <PasswordManager />
+        <button className={styles.Button} onClick={() => logout()}>
+            Logout
+        </button>
+    </div>
+)
 
 const logout = async () => {
     const response = await fetch("/api/logout")
-    const res: LogoutAPI = await response.json()
-    if (res.ok) Router.push("/login")
+    if (response.ok) Router.push("/login")
 }
 
 export default AdminPage
