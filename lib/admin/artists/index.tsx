4f1e83529e720a10ae97adec887c0956f81a492f
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-03-01 23:08:51 +0500

    Artists: improve error messages and form design

--- lib/admin/artists/index.tsx
+++ lib/admin/artists/index.tsx
@@ -1,14 +1,11 @@
 import People from "./people"
 import Roles from "./roles"
-// import styles from "./artists.module.css"
 
-const Artists = () => {
-    return (
-        <div>
-            <People />
-            <Roles />
-        </div>
-    )
-}
+const Artists = () => (
+    <div>
+        <People />
+        <Roles />
+    </div>
+)
 
 export default Artists
